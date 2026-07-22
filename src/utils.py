7f362de4c6304c36a6e5cc207d87ae7305a02afb
anamika <anamika.godara1302@gmail.com>
2026-07-22 09:02:57 +0530

    Building prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 import os
 import sys
+from altair import param
 import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
-
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -17,21 +18,36 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(models)):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            #model.fit(X_train,y_train)
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            y_test_pred=model.predict(X_test)
+            y_test_pred=gs.predict(X_test)
 
             test_model_score=r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]]=test_model_score
 
         return report
+    
 
     except Exception as e:
-        raise CustomException(e,sys)    
+        raise CustomException(e,sys) 
+
+def load_object(file_path):
+        try:
+            with open(file_path,'rb') as file_obj:
+                return dill.load(file_obj)
+        except Exception as e:
+            raise CustomException(e,sys)       
